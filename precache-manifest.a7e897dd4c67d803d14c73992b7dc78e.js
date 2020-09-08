1cb5f173980fe2982046b1431af6b27fa752c529
z-igor <z-ig0rGITHUB>
2020-09-08 13:44:31 +0300

    Updates

---
+++ precache-manifest.a7e897dd4c67d803d14c73992b7dc78e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfb779f06a91fcf59aa0d80eadec3c2",
+    "url": "/foreman_gh-ps/index.html"
+  },
+  {
+    "revision": "6610dcf14eae831f7d31",
+    "url": "/foreman_gh-ps/static/css/main.d1aed2c1.chunk.css"
+  },
+  {
+    "revision": "ce333454b36e8d33b0db",
+    "url": "/foreman_gh-ps/static/js/2.b6bf243b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/foreman_gh-ps/static/js/2.b6bf243b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6610dcf14eae831f7d31",
+    "url": "/foreman_gh-ps/static/js/main.b8aba2a6.chunk.js"
+  },
+  {
+    "revision": "a2e1b503c06382f45095",
+    "url": "/foreman_gh-ps/static/js/runtime-main.c212d72c.js"
+  },
+  {
+    "revision": "165c5992fe4d959210f6d8fced325a38",
+    "url": "/foreman_gh-ps/static/media/avatarTeam.165c5992.png"
+  },
+  {
+    "revision": "013825018c8e63383ef5a409a21327dd",
+    "url": "/foreman_gh-ps/static/media/logo.01382501.svg"
+  },
+  {
+    "revision": "580b9bc9ce8f36172af36f7913cc53de",
+    "url": "/foreman_gh-ps/static/media/notification.580b9bc9.svg"
+  },
+  {
+    "revision": "0707e670ab6b30303c96d3dac43b4b53",
+    "url": "/foreman_gh-ps/static/media/triangle.0707e670.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/foreman_gh-ps/precache-manifest.a7e897dd4c67d803d14c73992b7dc78e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/foreman_gh-ps/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
